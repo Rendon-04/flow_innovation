7ad1fd47ca094ac05884dbca823b2ec70dac2a0e
Israel <bamideleisrael04@gmail.com>
2025-01-24 19:19:41 +0100

    Add main Flask application entry point to initialize the app
    This file contains the entry point for the application, where the Flask instance is created and configured. It ensures all routes, blueprints, and configurations are loaded correctly when the app starts.

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,25 @@
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
+from extensions import db 
+# Import Blueprints
+from routes import fact_checker
+from models import initialize_database
 
+
+# Initialize Flask app and database
 app = Flask(__name__)
 CORS(app)
 app.config.from_object("config")
-#Set the database URI and disable track modifications warning    TODO
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # or use your actual DB URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-# Import routes
-from routes import *
+# Initialize extensions
+db.init_app(app)  # Bind SQLAlchemy to the app
+migrate = Migrate(app, db)  # Bind Flask-Migrate to the app and db
+
+# Register Blueprints
+app.register_blueprint(fact_checker)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.config['ENV'] = 'development'
+    #app.run(debug=os.getenv("FLASK_DEBUG", False))  # Use environment variable for debug mode
